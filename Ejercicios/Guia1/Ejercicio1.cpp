89fbe82dabef3c7f0574cc39923574f15789d931
Melina Loza <129029554+melinaloza@users.noreply.github.com>
2024-01-23 21:56:58 -0300

    Add files via upload

---
+++ Ejercicios/Guia1/Ejercicio1.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a=0;
+    int b=0;
+
+    cout << "Ingrese valor A" << endl;
+    cin >> a;
+    cout << "Ingrese valor B" << endl;
+    cin >> b;
+
+    int suma = a+b;
+    int resta= a-b;
+    int producto= a*b;
+    int division=a/b;
+
+    cout << "Resultado Suma: " << suma << endl;
+    cout << "Resultado Resta: " << resta << endl;
+    cout << "Resultado Producto: " << producto << endl;
+
+    if (b==0){
+        cout << "No puede dividir por cero" << endl;
+        return 1;
+    }else{
+        cout << "Resultado Division: " << division << endl;
+    }
+
+    return 0;
+}
